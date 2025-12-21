c0d6b4e11b6c4fb04ac09eaac21cda61180694a4
Sylvester Obikwelu <theopensly@gmail.com>
2025-12-21 11:11:04 +0100

    added clear-cache

--- routes/web.php
+++ routes/web.php
@@ -66,4 +66,12 @@ Route::get('storage-link', function () {
     return 'Storage link created.';
 })->name('storage-link');
 
+Route::get('clear-cache', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+    return 'Cache cleared successfully.';
+})->name('clear-cache');
+
 require __DIR__.'/auth.php';
